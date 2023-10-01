84805796fe174e8fa888e7f8f81b2e2b32ccf215
light <caiocr610@gmail.com>
2023-10-01 01:16:09 -0300

    Implementei o loop principal do jogo, com alguns testes. Organizei os arquivos em pastas mais específicas. Para abriro jogo basta rodar o arquivo main.py. Obs: adicionando pastas que nao foram no ultimo add

---
+++ prototipo/data/entities/TankSeeker.py
@@ -0,0 +1,8 @@
+from seekers import Seeker
+
+class TankSeeker(Seeker):
+    def __init__(self):
+        super().__init__()
+    
+    def increment_armor(self) -> None:
+        pass
---
+++ prototipo/main.py
@@ -0,0 +1,9 @@
+from app import App
+
+
+# Função para rodar o jogo
+def main() -> None:
+    App().run()
+
+if __name__ == '__main__':
+    main()

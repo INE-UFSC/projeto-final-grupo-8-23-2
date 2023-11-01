18aa3c8992f46ace88886992d99c0e8987efb00f
Emanuelle <emanuguse@gmail.com>
2023-10-31 23:15:08 -0300

    alteracao path

--- versao_final/src/map/map.py
+++ versao_final/src/map/map.py
@@ -1,11 +1,13 @@
 import pygame
 import math
 from constants import game_constants
+from utils.utils import get_file_path
 
 class Map:
     def __init__(self) -> None:
         self.__size = 150
-        self.__image = pygame.transform.scale(pygame.image.load('versao_final/resources/purplegrass.webp'), (self.__size, self.__size))
+        resources_path = get_file_path(__file__)
+        self.__image = pygame.transform.scale(pygame.image.load(f'{resources_path}/textures/purplegrass.webp'), (self.__size, self.__size))
         self.__width = game_constants.SCREEN_WIDTH
         self.__height = game_constants.SCREEN_HEIGHT
         

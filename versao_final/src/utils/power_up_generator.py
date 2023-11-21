5c99ab43db1fda2f49e484a635783474ac95eb02
Erika <erikaamericano393@gmail.com>
2023-11-21 11:00:35 -0300

    update branch main

--- versao_final/src/utils/power_up_generator.py
+++ versao_final/src/utils/power_up_generator.py
@@ -5,7 +5,6 @@ import random
 from powerups import power_up, power_up_health, power_up_speed
 from entities import player
 
-
 class PowerUpGenerator:
     def __init__(self, power_up_list_ref: list[power_up.PowerUp], player_ref: player.Player) -> None:
         self.__power_ups = power_up_list_ref

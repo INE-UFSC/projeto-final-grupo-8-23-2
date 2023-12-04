1d69b76d1a45bd63782b0273b28c7d7b8fe4aac7
light <caiocr610@gmail.com>
2023-12-04 18:18:10 -0300

    removendo o powerup das armas por conta do bug

--- versao_final/src/utils/power_up_generator.py
+++ versao_final/src/utils/power_up_generator.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 
 import random
 
-from powerups import power_up, power_up_health, power_up_speed, power_up_weapon
+from powerups import power_up, power_up_health, power_up_speed
 from entities import player
+
 
 class PowerUpGenerator:
     def __init__(self, power_up_list_ref: list[power_up.PowerUp], player_ref: player.Player) -> None:
@@ -13,7 +14,7 @@ class PowerUpGenerator:
     def generate(self) -> None:
         power_up = random.choice([
             power_up_health.PowerUpHealth(self.__player),
-            power_up_speed.PowerUpSpeed(self.__player),
-            power_up_weapon.PowerUpWeapon(self.__player)
+            power_up_speed.PowerUpSpeed(self.__player)
         ])
         self.__power_ups.append(power_up)
+
